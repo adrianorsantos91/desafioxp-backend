37f9873cfaa1ddbc86276281aab5f62955b3d2b5
adrianorsantos <adriano.adm2010@gmail.com>
2022-07-17 18:53:17 -0300

    refactor(seeders:assets): changed the table reference and value type associated with the keys

--- src/database/seeders/20220717195147-assets.js
+++ src/database/seeders/20220717195147-assets.js
@@ -1,26 +1,26 @@
 module.exports = {
   up: async (queryInterface, _Sequelize) => {
-    await queryInterface.bulkinsert('Users',
+    await queryInterface.bulkinsert('Assets',
     [{
       id: 1,
-      quantityAssets: '1000',
+      quantityAssets: 1000,
       price: 50.00
     },
     {
       id: 2,
-      quantityAssets: '2000',
+      quantityAssets: 2000,
       price: 20.00
     },
     {
       id: 3,
-      quantityAssets: '500',
+      quantityAssets: 500,
       price: 5.00
     },
   ])
   },
 
   down: async (queryInterface, _Sequelize) => {
-    await queryInterface.bulkDelete('Users', null, {});
+    await queryInterface.bulkDelete('Assets', null, {});
   },
 };
 
