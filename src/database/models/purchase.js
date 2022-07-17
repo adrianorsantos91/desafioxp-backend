38e85438a29a00c78b3598b269f7f170897c41e3
adrianorsantos <adriano.adm2010@gmail.com>
2022-07-17 14:39:59 -0300

    chore(purchase): created the purchase model to store customer purchase records

---
+++ src/database/models/purchase.js
@@ -0,0 +1,29 @@
+const Stock = (sequelize, DataTypes) => {
+  const Stock = sequelize.define("Stock", {
+    id: {
+      type: DataTypes.INTEGER,
+      primarykey: true,
+      autoIncrement: true
+    },
+    userId: {
+      type: DataTypes.INTERGER,
+      foreignkey: true
+    },
+    stockId:{
+      type: DataTypes.INTERGER,
+      foreignkey: true
+    },
+    qtdeAtivo: DataTypes.STRING,
+    valor: DataTypes.STRING
+  }, { timestamps: false
+  });
+
+  Stock.associate = (models) => {
+    Stock.belongsTo(models.User,
+      { foreignkey: 'userId', as: 'user' });
+  };
+
+  return Stock;
+}
+
+module.exports = Stock;
