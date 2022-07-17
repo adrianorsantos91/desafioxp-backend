cda3f2c41f778ae745a6b606c896a07b4347fa8b
adrianorsantos <adriano.adm2010@gmail.com>
2022-07-17 14:37:20 -0300

    chore(user): created the model user and made an association with the purchase table

---
+++ src/database/models/user.js
@@ -0,0 +1,20 @@
+const User = (sequelize, DataTypes) => {
+  const User = sequelize.define("User", {
+    id: {
+      type: DataTypes.INTEGER,
+      primarykey: true,
+      autoIncrement: true
+    },
+    saldo: DataTypes.FLOAT,
+  }, { timestamps: false
+  });
+
+  User.associate = (models) => {
+    User.hasMany(models.Purchase,
+      { foreignkey: 'userId', as: 'purchases'});
+  };
+
+  return User;
+}
+
+module.exports = User;
