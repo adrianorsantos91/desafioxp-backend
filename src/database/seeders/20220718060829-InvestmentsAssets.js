9eaafdcbda67a00a330d092d2d29ef3eef65cbe3
adrianorsantos <adriano.adm2010@gmail.com>
2022-07-18 03:18:54 -0300

    chore(seeders:investmentsassets): created to populate the initial data in the intermediate table

---
+++ src/database/seeders/20220718060829-InvestmentsAssets.js
@@ -0,0 +1,26 @@
+module.exports = {
+  up: async (queryInterface, _Sequelize) => {
+    await queryInterface.bulkinsert('InvestmentAssets',
+    [
+      {
+        investmentId: 1,
+        assetId: 1,
+      },
+      {
+        investmentId: 2,
+        assetId: 2,
+      },
+      {
+        investmentId: 3,
+        assetId: 3,
+      },
+
+    ], { timestamps: false });
+  },
+
+  down: async (queryInterface, _Sequelize) => {
+    await queryInterface.bulkDelete('InvestmentAssets', null, {});
+  },
+};
+
+
