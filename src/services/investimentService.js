226f15816e3f9ba6ef31a1f3a5225b4793a9db78
adrianorsantos <adriano.adm2010@gmail.com>
2022-07-19 13:43:54 -0300

    chore(purchaseassets): implemented to a function for purchasing assets

--- src/services/investimentService.js
+++ src/services/investimentService.js
@@ -1,17 +1,36 @@
-const { User, Asset } = require('../database/models');
+const { User, Asset, Investment } = require('../database/models');
 
 /* Validação: Quantidade de ativo a ser comprada não pode ser maior que a quantidade disponível na corretora */
 
-const createdPurchase = async (email, { _codCliente, codAtivo, _qtdeAtivo }) => {
-  const user = await User.findOne({ where: { email } });
-  console.log('asset', user);
+const createdPurchase = async ({ codCliente, codAtivo, qtdeAtivo }) => {
+  const user = await User.findOne({ where: { id: codCliente } });
+
   const { count } = await Asset.findAndCountAll({ where: { id: codAtivo } });
-  console.log('asset', count);
+
   if (count === 0) return false;
 
   const asset = await Asset.findOne({ where: { id: codAtivo } });
-  console.log('asset', asset);
-  return user;
+
+  if (asset.quantityAsset < qtdeAtivo) return 'quantity';
+
+  if (user.amount < qtdeAtivo * asset.price) return 'amount';
+
+  const investment = await Investment.create({
+    userId: codCliente, assetId: codAtivo, quantityAsset: qtdeAtivo });
+    console.log('investments: %s', investment.id);
+    console.log('userId: %s', investment.userId);
+    console.log('assetId: %s', investment.assetId);
+    console.log('quantityAsset: %s', investment.quantityAsset);
+
+  /* await InvestmentAsset.create({ assetId: codAtivo, investmentId: investment.id }); */
+
+  await Asset.update({ quantityAsset: asset.quantityAsset - qtdeAtivo },
+    { where: { id: codAtivo } });
+
+  await User.update({ amount: user.amount - qtdeAtivo * asset.price },
+    { where: { id: codCliente } });
+
+  return asset;
 };
 
 module.exports = {
