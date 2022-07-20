e9434a4b46e0e0e462fdae630fc05d792553b656
adrianorsantos <adriano.adm2010@gmail.com>
2022-07-20 04:26:03 -0300

    refactor(api): added customer route to /clientes

--- src/api.js
+++ src/api.js
@@ -1,11 +1,13 @@
 const express = require('express');
 // require('express-async-errors');
 const investmentRouters = require('./routers/investmentRouters');
+const clienteRouters = require('./routers/clienteRouters');
 
 const app = express();
 
 app.use(express.json());
 
 app.use('/investimentos', investmentRouters);
+app.use('/clientes', clienteRouters);
 
 module.exports = app;
