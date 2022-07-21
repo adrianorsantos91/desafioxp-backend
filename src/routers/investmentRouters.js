12610a093fcfc17b900150bbacdaff01592de261
adrianorsantos <adriano.adm2010@gmail.com>
2022-07-21 18:24:59 -0300

    refactor(routers): added validations in post sale and purchase routes

--- src/routers/investmentRouters.js
+++ src/routers/investmentRouters.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const investmentController = require('../controllers/investmentController');
+const { validateContainsFields } = require('../middleware/validateInvestment');
 // const authenticationMiddleware = require('../middlewares/auth.middleware');
 
 const router = express.Router();
 
-router.post('/comprar', investmentController.createdPurchase);
-router.post('/vender', investmentController.createdSale);
+router.post('/comprar', validateContainsFields, investmentController.createdPurchase);
+router.post('/vender', validateContainsFields, investmentController.createdSale);
 
 module.exports = router;
