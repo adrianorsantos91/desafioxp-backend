ec4d1cf9a99bd97b08df1d7ce3e9fe3cc8f88360
adrianorsantos <adriano.adm2010@gmail.com>
2022-07-21 18:28:42 -0300

    refactor(routers): added route /clients/{id} to list clients by "userId"

--- src/routers/assetRouters.js
+++ src/routers/assetRouters.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const assetController = require('../controllers/assetController');
+const clientController = require('../controllers/clientController');
 // const authenticationMiddleware = require('../middlewares/auth.middleware');
 
 const router = express.Router();
 
-router.get('/ativos/:id', assetController.getAssetById);
+router.get('/:id', assetController.getAssetById);
+router.get('/clientes/:id', clientController.getClientById);
 
 module.exports = router;
