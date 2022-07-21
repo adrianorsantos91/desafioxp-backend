991a4f73f78b6458b1b7a72592ce6eff403e7b85
adrianorsantos <adriano.adm2010@gmail.com>
2022-07-21 13:59:22 -0300

    feat(src/services): implemented the share sale function and adjusted the purchase function

--- src/services/investimentService.js
+++ src/services/investimentService.js
@@ -1,32 +1,47 @@
 const { Op } = require('sequelize');
-const { Asset, Investment, InvestmentAsset } = require('../database/models');
+const { User, Asset, Investment, InvestmentAsset } = require('../database/models');
 /* Validação: Quantidade de ativo a ser comprada não pode ser maior que a quantidade disponível na corretora */
 
 const checkUserWithAsset = async (investWithAsset, asset, { userId, assetId, quantityAsset }) => {
   const [result] = investWithAsset.filter((inv) =>
   inv.userId === userId && inv.assets[0].id === assetId);
-
+  const user = await User.findOne({ where: { id: userId } });
   if (!result) {
-    const newInvestment = await Investment.create({
-      userId,
-      quantityAsset,
-      price: asset.price });
+    const newInvestment = await Investment.create({ userId, quantityAsset, price: asset.price });
     await InvestmentAsset.create({ investmentId: newInvestment.id, assetId });
   }
 
   if (result) {
     const averagePrice = (result.quantityAsset * result.price
       + asset.price * quantityAsset) / (result.quantityAsset + quantityAsset);
-    await Investment.update({
-      userId,
+    await Investment.update({ userId,
       quantityAsset: result.quantityAsset + quantityAsset,
       price: averagePrice }, {
       where: { [Op.and]: [{ id: result.id }, { userId }] } });
+   const amount = parseFloat(user.amount) - parseFloat(quantityAsset * asset.price);
+   console.log('amountAtual: %s', amount);
+   await User.update({ amount }, { where: { id: userId } }); // Atualiza o saldo do cliente;
   }
+};
+
+const checkAssetToSale = async (investWithAsset, asset, { userId, assetId, quantityAsset }) => {
+  const [result] = investWithAsset.filter((inv) =>
+  inv.userId === userId && inv.assets[0].id === assetId);
+  if (result.quantityAsset < quantityAsset) return false;
+  const user = await User.findOne({ where: { id: userId } });
+  await Investment.update({
+    quantityAsset: result.quantityAsset - quantityAsset,
+    }, {
+    where: { [Op.and]: [{ id: result.id }, { userId }] } });
+    const amount = parseFloat(user.amount) + parseFloat(quantityAsset * asset.price);
+   console.log('amountAtual: %s', amount);
+   await User.update({ amount }, { where: { id: userId } });
+  return result;
 };
 
 const createdPurchase = async (purchase) => {
   const { userId, assetId, quantityAsset } = purchase;
+
   const { count } = await Asset.findAndCountAll({ where: { id: assetId } });
   if (count === 0) return false;
   const asset = await Asset.findOne({ where: { id: assetId } });
@@ -48,13 +63,34 @@ const createdPurchase = async (purchase) => {
     };
 };
 
-// const createdSale = async ({ userId, assetId, quantityAsset }) => {
+const createdSale = async (sale) => {
+  const { userId, assetId } = sale;
+  const { count } = await Asset.findAndCountAll({ where: { id: assetId } });
+  if (count === 0) return false;
+  const asset = await Asset.findOne({ where: { id: assetId } });
 
-// };
+  const investWithAsset = await Investment.findAll({ where: { userId },
+    include: { model: Asset,
+      as: 'assets',
+    attributes: { exclude: ['quantityAsset'] },
+    through: { attributes: [] },
+  } });
+
+  const result = await checkAssetToSale(investWithAsset, asset, sale);
+
+  if (!result) return 'quantity';
+
+    return {
+      id: asset.id,
+      userId,
+      quantityAsset: parseInt(asset.quantityAsset, 10),
+      price: parseFloat(asset.price),
+    };
+};
 
 module.exports = {
   createdPurchase,
-  // createdSale,
+  createdSale,
 };
 
 // // const user = await User.findOne({ where: { id: userId } });
