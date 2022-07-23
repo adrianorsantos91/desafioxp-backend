538614d66f5a3d48f6ebe176f17100e8891d80e4
adrianorsantos <adriano.adm2010@gmail.com>
2022-07-23 03:41:53 -0300

    refactor(errorhandler): added one more status and message

--- src/utils/errorMessage.js
+++ src/utils/errorMessage.js
@@ -2,10 +2,12 @@ const notFoundAsset = { status: 400, message: 'Asset not found' };
 const errorQuantity = { status: 400, message: '"quantityAsset" is not available' };
 const errorAmount = { status: 400, message: '"amount" is not available' };
 const notFoundClient = { status: 400, message: 'Client not found' };
+const errorQuantityWallet = { status: 400, message: '"quantityAsset" unavailable in wallet' };
 
 module.exports = {
  notFoundAsset,
  errorQuantity,
  errorAmount,
  notFoundClient,
+ errorQuantityWallet,
 };
